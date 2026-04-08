d09f9df30b370884d85438abb7e2e9ed74e4522f
Cooli <sammy.hendry29@icloud.com>
2026-04-08 17:46:46 +0100

    ok

--- data/porcellum/script.lua
+++
@@ -1,9 +0,0 @@
-function onCreatePost()
-	addCharacterToList('Piglet_Porcellum', 'dad')
-end
-
-function onStepHit()
-	if curStep == 288 then
-		triggerEvent('Change Character', 'dad', 'Piglet_Porcellum')
-	end
-end
